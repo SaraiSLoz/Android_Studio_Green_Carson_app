c7b2b92734a4c861c322913e117915b75f5a9dc8
Sarai Santiago Lozano <sarailoza05@gmail.com>
2023-11-13 01:35:37 -0600

    design pages

--- Android_Studio/app/src/main/java/com/example/bit_3/menu.java
+++ Android_Studio/app/src/main/java/com/example/bit_3/menu.java
@@ -1,5 +1,6 @@
 package com.example.bit_3;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -8,10 +9,16 @@ import androidx.appcompat.app.AppCompatActivity;
 public class menu extends AppCompatActivity {
 
     Button Usuario, Recolectores, Centros, Residuos;
-    //
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+        Usuario = findViewById(R.id.Usuario_but);
+        Recolectores = findViewById(R.id.Recolectores_but);
+        Centros = findViewById(R.id.Centros_but);
+        Residuos = findViewById(R.id.Residuos_but);
+
+
         Usuario.setOnClickListener(v -> setContentView(R.layout.usuarios));
         Recolectores.setOnClickListener(v -> setContentView(R.layout.recolectores));
         Centros.setOnClickListener(v -> setContentView(R.layout.centros));
